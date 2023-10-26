df67b87d6e82d4ca95be318cd9625b2ee1456757
Your Name <you@example.com>
2023-10-26 22:04:32 +0500

    menu maodal

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
+console.log(document.querySelectorAll('h5'), 'fuck');
 // Variables to store user selections
 let selectedPizza = '';
 let selectedSize = '';
-let selectedCrust = '';
-let selectedExtras1 = '';
-let selectedExtras2 = '';
+let selectedcheese = '';
+let selectedMeat = '';
+let selectedVeggie = '';
+let selectedDipping = '';
 
 // Function to update the pizza details
 function updatePizzaDetails() {
     document.getElementById('pizzaName').textContent = `Pizza: ${selectedPizza}`;
     document.getElementById('pizzaSize').textContent = `Size: ${selectedSize}`;
-    document.getElementById('pizzaCrust').textContent = `Crust: ${selectedCrust}`;
-    document.getElementById('pizzaExtras1').textContent = `Extras 1: ${selectedExtras1}`;
-    document.getElementById('pizzaExtras2').textContent = `Extras 2: ${selectedExtras2}`;
+    document.getElementById('pizzacheese').textContent = `Cheese: ${selectedcheese}`;
+    document.getElementById('Meat').textContent = `Meat : ${selectedMeat}`;
+    document.getElementById('Veggie').textContent = `Veggie : ${selectedVeggie}`;
+    console.log('hell,', document.getElementById('Veggie').textContent);
+    document.getElementById('Dipping').textContent = `Dipping: ${selectedDipping}`;
 }
 
 // Function to reset selections
 function resetSelections() {
     selectedPizza = '';
     selectedSize = '';
-    selectedCrust = '';
-    selectedExtras1 = '';
-    selectedExtras2 = '';
+    selectedcheese = '';
+    selectedMeat = '';
+    selectedVeggie = '';
+    selectedDipping = '';
     updatePizzaDetails();
 }
 
@@ -42,29 +47,38 @@ sizeCards.forEach((card, index) => {
     });
 });
 
-// Step 3: Listen for the crust and extras selections
-const crustAndExtrasCards = document.querySelectorAll('.card-crust');
-crustAndExtrasCards.forEach((card, index) => {
+// Step 3: Listen for the cheese selections
+const cheeseCards = document.querySelectorAll('.card-cheese');
+cheeseCards.forEach((card, index) => {
     card.addEventListener('click', () => {
-        selectedCrust = card.querySelector('.card-text').textContent;
+        selectedcheese = card.querySelector('.card-text').textContent;
         updatePizzaDetails();
     });
 });
 
-// Step 4: Listen for the extras 1 selections
-const extraCards = document.querySelectorAll('.card-extra');
+// Step 4: Listen for the Meat  selections
+const extraCards = document.querySelectorAll('.card-meat');
 extraCards.forEach((card, index) => {
     card.addEventListener('click', () => {
-        selectedExtras1 = card.querySelector('.card-text').textContent;
+        selectedMeat = card.querySelector('.card-text').textContent;
         updatePizzaDetails();
     });
 });
 
-// Step 5: Listen for the extras 2 selections
+// Step 5: Listen for the Veggie  selections
+const VeggieCards = document.querySelectorAll('.card-veggie');
+VeggieCards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        selectedVeggie = card.querySelector('.card-text').textContent;
+        updatePizzaDetails();
+    });
+});
+
+// Step 6: Listen for the Dipping  selections
 const dippingCards = document.querySelectorAll('.card-dipping');
 dippingCards.forEach((card, index) => {
     card.addEventListener('click', () => {
-        selectedExtras2 = card.querySelector('.card-text').textContent;
+        selectedDipping = card.querySelector('.card-text').textContent;
         updatePizzaDetails();
     });
 });
@@ -76,3 +90,42 @@ closeModalButton.addEventListener('click', () => {
 });
 
 
+
+
+
+
+
+
+// Add an event listener to the Close button
+document.getElementById('close-modal').addEventListener('click', function () {
+    const selectedcheese = arguments/* code to check if a cheese is selected */; // Replace with your logic
+    const selectedMeat = arguments /* code to check if an extra is selected */; // Replace with your logic
+    const selectedVeggie = arguments /* code to check if an extra is selected */; // Replace with your logic
+    const selectedDipping = arguments/* code to check if a sauce is selected */; // Replace with your logic
+
+
+    if (selectedcheese && selectedMeat && selectedVeggie && selectedDipping) {
+        $('#exampleModal-1').modal('hide'); // Close the modal using Bootstrap modal functions
+    } else {
+        alert('Please select one option from each category before closing.');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+    console.log(abdullah);
+
+    cards.forEach(function (card) {
+        card.addEventListener("click", function () {
+            // Remove the "selected-card" class from all cards
+            cards.forEach(function (c) {
+                c.classList.remove("selected-card");
+            });
+
+            // Add the "selected-card" class to the clicked card
+            card.classList.add("selected-card");
+        });
+    });
+
+
+});
